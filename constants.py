b228057efe9c75d35702c641ae726844ea2d93c6
Yonnas Araya <yonnas.a24@hotmail.com>
2021-05-05 18:47:57 +1000

    created new module for constants and created methods to page forward and backwards through list of tickets

---
+++ constants.py
@@ -0,0 +1,23 @@
+class Constants:
+
+    DETAILED_TICKET_ATTRIBUTES = [
+        'id',
+        'subject',
+        'requester_id',
+        'assignee_id',
+        'created_at',
+        'description',
+        'priority',
+        'status'
+
+    ]
+
+    SUMMARY_TICKET_ATTRIBUTES = [
+        'id',
+        'subject',
+        'requester_id',
+        'assignee_id',
+    
+    ]
+
+    PAGINATION_SIZE = {'page[size]': 2}
